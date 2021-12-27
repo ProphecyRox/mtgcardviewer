2c5d5a4f7b91add8b35fe72e37dd9a0071bff37d
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2021-12-26 22:31:54 -0800

    only display textual info for now

--- viewer/urls.py
+++ viewer/urls.py
@@ -3,4 +3,5 @@ from viewer import views
 
 urlpatterns = [
     path('', views.index),
+    path('details/<str:name>/', views.details)
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Card
+from django.template import loader #, render
 
 def index(request):
-    return HttpResponse("MTG Card Viewer")
+    first_five_cards = Card.objects.all()[:5]
+    template = loader.get_template('viewer/index.html')
+    context = {
+        'first_five_cards': first_five_cards
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, name):
+    template = loader.get_template('viewer/details.html')
+    context = {
+        'name': name
+    }
+    return HttpResponse(template.render(context, request))
