2decc53ec59e00c2896a3b27eccbc5cf4e4050b4
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2022-01-02 22:36:13 -0800

    use render shortcut method for views

--- viewer/views.py
+++ viewer/views.py
@@ -1,32 +1,22 @@
 from django.http import HttpResponse
 from .models import Card
-from django.template import loader #, render
+from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 
 def index(request):
     first_five_cards = Card.objects.all()[:5]
-    template = loader.get_template('viewer/index.html')
-    context = {
-        'first_five_cards': first_five_cards
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'viewer/index.html', {'first_five_cards': first_five_cards})
 
 def details(request, id):
-    template = loader.get_template('viewer/details.html')
     try:
         card = Card.objects.get(id=id)
     except ObjectDoesNotExist:
         return HttpResponse("Error 404: Card not found")
 
-    context = {
-        'card': card,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'viewer/details.html', {'card': card})
 
 def contact(request):
-    template = loader.get_template('viewer/contact.html')
-    return HttpResponse(template.render({}, request))
+    return render(request, 'viewer/contact.html', {})
 
 def search(request):
-    template = loader.get_template('viewer/search.html')
-    return HttpResponse(template.render({}, request))
+    return render(request, 'viewer/search.html', {})
