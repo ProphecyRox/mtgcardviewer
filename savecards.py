6805fb4e5b73f6d414631e4631bf2d4bfa8b6110
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2022-01-02 19:19:50 -0800

    create script to save cards from json to db

---
+++ savecards.py
@@ -0,0 +1,26 @@
+import json
+from viewer.models import Card
+
+fp = open("ARN.json")
+data = json.load(fp)
+
+for card in data["data"]["cards"]:
+    print(f'Importing {card["name"]}...')
+    card_obj = Card(
+        name=card["name"],
+        mana_cost=card["convertedManaCost"],
+        card_type=card["type"],
+        ability_text=card["text"]
+    )
+
+    try:
+        card_obj.flavor_text = card["flavorText"]
+    except:
+        # Just don't set flavor text if card doesn't have one
+        pass
+
+    card_obj.save()
+
+fp.close()
+print("Done")
+
