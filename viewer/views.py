841aaf8eb286fb8181e88b2263db060fb48db100
Julian Nguyen <72328335+ProphecyRox@users.noreply.github.com>
2022-01-03 21:18:58 -0800

    form now works at a basic level

--- viewer/views.py
+++ viewer/views.py
@@ -3,6 +3,8 @@ from .models import Card
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 
+from .forms import SearchForm
+
 def index(request):
     first_five_cards = Card.objects.all()[:5]
     return render(request, 'viewer/index.html', {'first_five_cards': first_five_cards})
@@ -19,4 +21,28 @@ def contact(request):
     return render(request, 'viewer/contact.html', {})
 
 def search(request):
-    return render(request, 'viewer/search.html', {})
+    search_performed = False
+    search_results = Card.objects.none()
+
+    if request.method == "POST":
+        # Process form data
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            # Get search results
+            card_name = request.POST['name']
+            search_results = Card.objects.filter(name__contains=card_name)
+            # Set flag for template to use
+            search_performed = True
+    else:
+        # Show blank form
+        form = SearchForm()
+
+    # Render form
+    form_html = form.as_p()
+    return render(request, 'viewer/search.html',
+        {
+            'form_html': form_html,
+            'search_results': search_results,
+            'search_performed': search_performed
+        }
+    )
